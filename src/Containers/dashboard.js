17ce29e988f8ad14627b3f298560db54650bb770
jayanth babu somineni <jayanth@jayanths-MacBook-Air.local>
2018-12-23 23:38:02 +0530

    react-create-app on Heroku

--- src/Containers/dashboard.js
+++ src/Containers/dashboard.js
@@ -6,25 +6,9 @@ class Dashboard extends Component {
         mobiles: []
     }
     componentDidMount() {
-        fetch(apiroutes.mobiles)
-            .then(response => response.json())
-            .then(json => {
-                console.log(json);
-                this.setState({ mobiles: json })
-            })
+        
     }
     render() {
-        const data = this.state.mobiles.length && this.state.mobiles.map((value, index) => {
-            return (
-                <div class="col-sm-3"><div class="card" >
-                    <img class="card-img-top" height={300} width={300} src={require(`../Images/${value.url}.jpeg`)} alt="Card image cap" />
-                    <div class="card-body">
-                        <h5 class="card-title">{value.title}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div></div>)
-        })
         return (
             <React.Fragment>
                 <Header location={this.props.history.location}></Header>
