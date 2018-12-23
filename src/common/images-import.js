fe8e86c77c15c471c74a5c2c9ad311a6f9a2cb71
jayanth babu somineni <jayanth@jayanths-MacBook-Air.local>
2018-12-24 02:09:16 +0530

    react-create-app on H

--- src/common/images-import.js
+++ src/common/images-import.js
@@ -1,27 +1,27 @@
-import Search from '../Images/home/search.png';
-import Heart from '../Images/home/heart.png';
-import Cart from '../Images/home/cart.png';
-import banner_product from '../Images/banner_product.png';
-import char_1 from '../Images/home/char_1.png';
-import char_2 from '../Images/home/char_2.png';
-import char_3 from '../Images/home/char_3.png';
-import char_4 from '../Images/home/char_4.png';
-import best_1 from '../Images/home/best_1.png';
-import review_1 from '../Images/home/review_1.jpg';
-import review_2 from '../Images/home/review_2.jpg';
-import review_3 from '../Images/home/review_3.jpg';
-import view_2 from '../Images/home/view_2.jpg';
-import view_3 from '../Images/home/view_2.jpg';
-import view_4 from '../Images/home/view_2.jpg';
-import view_5 from '../Images/home/view_2.jpg';
-import view_6 from '../Images/home/view_2.jpg';
-import send from '../Images/home/send.png';
-import single_2 from '../Images/home/single_2.jpg';
-import single_3 from '../Images/home/single_3.jpg';
-import single_4 from '../Images/home/single_4.jpg';
+import Search from '../images/home/search.png';
+import Heart from '../images/home/heart.png';
+import Cart from '../images/home/cart.png';
+import banner_product from '../images/banner_product.png';
+import char_1 from '../images/home/char_1.png';
+import char_2 from '../images/home/char_2.png';
+import char_3 from '../images/home/char_3.png';
+import char_4 from '../images/home/char_4.png';
+import best_1 from '../images/home/best_1.png';
+import review_1 from '../images/home/review_1.jpg';
+import review_2 from '../images/home/review_2.jpg';
+import review_3 from '../images/home/review_3.jpg';
+import view_2 from '../images/home/view_2.jpg';
+import view_3 from '../images/home/view_2.jpg';
+import view_4 from '../images/home/view_2.jpg';
+import view_5 from '../images/home/view_2.jpg';
+import view_6 from '../images/home/view_2.jpg';
+import send from '../images/home/send.png';
+import single_2 from '../images/home/single_2.jpg';
+import single_3 from '../images/home/single_3.jpg';
+import single_4 from '../images/home/single_4.jpg';
 
 
-export const Images = {
+export const images = {
     Search,
     Heart,
     Cart,
